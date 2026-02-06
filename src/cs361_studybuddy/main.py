7f88a1ee70beb6398a3c1c91d73edb416ce36a7e
Anvesha Kumar <anv90@users.noreply.github.com>
2026-02-05 17:00:34 -0800

    added more text styling for command prompts with rich

--- src/cs361_studybuddy/main.py
+++ src/cs361_studybuddy/main.py
@@ -1,60 +1,61 @@
+from rich.box import SIMPLE_HEAVY
 from ssl import Options
 from loguru import logger
 #from art import *
 from rich import print
-from rich.prompt import Prompt
+from rich.prompt import Prompt, Confirm, IntPrompt
 #from typing import list
 #from rich import print
 from rich.table import Table
+from rich.console import Console
+import time
+console = Console()
 
-def download_task_list(task_list: list[str]) -> list[int, str]:
+def download_task_list(task_list: list[str]) -> list[str]:
     #find textfile in folder csv or text??
     #if not found, create file
     #read from file and add to list
     #return the list
     # if()
     try:
-        with open("task.txt", "r") as file:
+        with open("src/cs361_studybuddy/task.txt", "r") as file:
             line = file.readline()
             while line:
                 task_list.append(line)
                 line = file.readline()
     except FileNotFoundError:
-        return
-    # return 0
+        logger.error("file not found")
+        return task_list
+    else:
+        logger.info("file found")
+        return task_list
 
-def upload_task_list(task_list: list[str]) -> list[int, str]:
-    #find textfile in folder csv or text??
-    #if not found, create file
-    #read from file and add to list
-    #return the liust
-    with open("task.txt", "w") as file:
-        for line in task_list:
-            file.write(line)
-    return 0
+def upload_task_list(task_list: list[str]):
+    if(len(task_list) > 0):
+        with open("src/cs361_studybuddy/task.txt", "w") as file:
+            for line in task_list:
+                file.write(f"{line}\n")
+    #return 0
 
 def print_task_list(task_list: list[str]) -> None:
-    grid = Table.grid(expand=True)
-    grid.add_column()
-    grid.add_column()
-    for idx, task in enumerate(task_list):
-        grid.add_row(f"{idx}", f"{task}")
+    # grid = Table.grid(expand=True)
+    # grid.add_column()
+    # grid.add_column()
+    # for idx, task in enumerate(task_list,start=1):
+    #     grid.add_row(f"{idx}", f"{task}")
 
-    print(grid)
+    # console.print(grid)
+    table = Table(title="Task List", box=SIMPLE_HEAVY, min_width=10)
+    for idx, task in enumerate(task_list,start=1):
+        table.add_row(f"{idx}", f"{task}")
+    console.print(table)
     #for each print??
     return;
 
 def add_task(task_list: list[str]) -> None:
-    #print the prompt for add a task
-    #accept user input
-    # while loop 
-    #ask if they want to edit
-    #call edit if yes
-    #once out of loop, add task to list
-    # use len to find end of list to find new num for task
-    task: str = input("Adding a task! Type contents here:")
+    task: str = input("\nAdding a task! Type contents here:")
     task_list.append(task)
-    print(f" Task {task} added!")
+    console.print(f" Task [magenta]{task}[/magenta] added!")
 
     return;
 
@@ -62,20 +63,34 @@ def edit_task(task_list: list[str]) -> None:
     #ask for new string
     #get new string
     #return it
-    idx = input("Which task do you want to edit?")
-    old_task = task_list[idx]
-    print(f"old task: {old_task}")
+    #idx = IntPrompt.ask("Which task do you want to edit?")
+    while True:
+        idx = IntPrompt.ask("\nWhich task would you like to edit?")
+        if (idx > 0 and idx < len(task_list)):
+            break
+        else:
+            console.print("invalid option, try again", style="red")
+    
+    old_task = task_list[idx-1]
+    console.print(f"old task: {old_task}")
     task: str = input("new task: ")
-    task_list[idx] = task
+    task_list[idx-1] = task
     #return " "
 
 def delete_task(task_list: list[str]) -> None:
-    idx = input("Which task would you like to delete?")
-    print(f"Are you sure you want to delete task {task_list[idx]}?\n This action is irreversible!")
-    conf = input("Type Y for yes, N for no:")
+    while True:
+        idx = IntPrompt.ask("\nWhich task would you like to delete?")
+        if (idx > 0 and idx < len(task_list)):
+            break
+        else:
+            console.print("invalid option, try again", style="red")
+    
+    console.print(f"Are you sure you want to delete task [magenta]{task_list[idx-1]}[/magenta]? [red]This action is irreversible![/red]")
+    #conf = input("Type Y for yes, N for no:")
+    conf = Prompt.ask("Type Y for yes, N for no:", choices=["Y", "N"])
     match conf:
         case "Y":
-            task_list.pop(idx)
+            task_list.pop(idx-1)
         case "N":
             return
     #ask are you sure??
@@ -91,15 +106,17 @@ def render_help_page() -> None:
     #instructions
     #header task page
     #instructions
-    print("I Need Help!\nWelcome to Study Buddy!\nOn the home page:\nTo view your to do list, type v\nOnce you are viewing your to do list, type a to add to your list, or d to delete from your list\nTo add to your to do list, type a\nQuestions? Contact help@studybuddy.com!")
+    console.print("I Need Help!\nWelcome to Study Buddy!\nOn the home page:\nTo view your to do list, type v\nOnce you are viewing your to do list, type a to add to your list, or d to delete from your list\nTo add to your to do list, type a\nQuestions? Contact help@studybuddy.com!")
     return;
 
 #use rich prompting!!!
 def open_task_page(task_list: list[str]) -> None:
     while True:
-        print(task_list)
-        print("Want to work on your list? a - add a task\nd - delete a task\ne - edit a task\nexit - exit to do list")
-        option = input("enter command here:")
+        time.sleep(0.5)
+        print_task_list(task_list)
+        print("\nWant to work on your list?\n[magenta bold]a[/magenta bold] - add a task\n[magenta bold]d[/magenta bold] - delete a task\n[magenta bold]e[/magenta bold] - edit a task\n[red bold]exit[/red bold] - exit to do list\n")
+        #option = input("enter command here:")
+        option : str = Prompt.ask("enter command here", choices=["a", "d", "e", "exit"])
         match option:
             case "a":
                 add_task(task_list)
@@ -108,7 +125,8 @@ def open_task_page(task_list: list[str]) -> None:
             case "e":
                 edit_task(task_list)
             case "exit":
-                print("Exiting Task List!")
+                console.print("\nExiting Task List!", style="bold")
+                console.rule(style="magenta")
                 break;
     #while loop
     #print list
@@ -118,16 +136,17 @@ def open_task_page(task_list: list[str]) -> None:
 
 
 def main() -> None:
-    logger.info("template-project")
-    print("welcome to study buddy")
+    #logger.info("template-project")
+    console.print("welcome to study buddy") #make this look pretty
 
     task_list: list[str] = []
     download_task_list(task_list)
-    print("A study tool to help you keep track of your tasks so you can have a more efficient study session!")
+    console.print("[bold]A study tool to help you keep track of your tasks so you can have a more efficient study session![/bold]\n")
     while True:
-        print("Type the command and press enter to navigate to the page!")
-        print("v - view to do list\nh - help page\na - add a task to list\nexit - exit studdy buddy")
-        option: str = input("enter command here:")
+        console.print("Type the command and press enter to navigate to the page!", style = "bold")
+        console.print("\n[magenta bold]v[/magenta bold] - view to do list\n[magenta bold]h[/magenta bold] - help page\n[magenta bold]a[/magenta bold] - add a task to list\n[red bold]exit[/red bold] - exit studdy buddy\n")
+        option: str = Prompt.ask("enter command here", choices=["v", "h", "a", "exit"])
+        console.rule(style="magenta")
         match (option):
             case "v":
                 open_task_page(task_list)
